f1d5ed3a9f384bd99bb9b7572d17a5c17bfbb75a
Ragul116 <ragulranjith1106@gmail.com>
2025-05-19 20:48:56 +0530

    image change

--- assets/JS/adoptionForm.js
+++ assets/JS/adoptionForm.js
@@ -12,3 +12,31 @@
       modal.style.display = "none";
     }
   }
+
+  function validateForm() {
+    const name = document.getElementById("name").value.trim();
+    const phone = document.getElementById("phone").value.trim();
+    const reason = document.getElementById("reason").value.trim();
+    const email = document.getElementById("email").value.trim();
+
+    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    const phonePattern = /^[0-9]{10}$/;
+
+    if (!name || !phone || !reason || !email) {
+      alert("Please fill out all fields.");
+      return;
+    }
+
+    if (!phonePattern.test(phone)) {
+      alert("Please enter a valid 10-digit phone number.");
+      return;
+    }
+
+    if (!emailPattern.test(email)) {
+      alert("Please enter a valid email address.");
+      return;
+    }
+
+
+    openThankYouModal(); 
+  }
